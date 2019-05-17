23ee9bc65e592a0ec9e70b8b50640dd87132843f
Majid <mohammadmajid55@gmail.com>
2019-05-17 18:14:38 +0000

    fixed env files

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,3 +1,4 @@
 class Admin::BaseController < ApplicationController
-    http_basic_authenticate_with name: "jungle", password: "book"
+    http_basic_authenticate_with name: ENV['ADMIN_USER'].to_s, password: ENV['ADMIN_PASS'].to_s
+
 end
