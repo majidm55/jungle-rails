4cafc32c0247bae3802b07cb137a0c078ce50c51
Majid <mohammadmajid55@gmail.com>
2019-05-18 01:37:03 +0000

    adding delete functionality-work in progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @reviews = Review.where(product_id: params[:id])
+    @sort = Review.all.order(created_at: :desc).where(product_id: params[:id])
     @review = Review.new
   end
 
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,8 @@
 class ReviewsController < ApplicationController
+
+  before_action :delete_review, :only => [:destroy]
+  before_action :new_review, :only => [:create]
+
   def create
     @product = Product.find(params[:product_id])
     @review = @product.reviews.create(review_params)
@@ -10,9 +14,23 @@ class ReviewsController < ApplicationController
       redirect_to @product, notice: "There was an error creating the comment: #{@review.errors.full_messages}"
     end
   end
- 
+  
+  def destroy
+    @delete.destroy
+    redirect_to product_path(@delete.product_id)
+  end
+
   private
     def review_params
       params.require(:review).permit(:description, :rating)
     end
+
+    def delete_review
+      @delete = Review.find params[:id]
+    end
+
+    def new_review
+      @review = Review.new(review_params)
+  end
+
 end
